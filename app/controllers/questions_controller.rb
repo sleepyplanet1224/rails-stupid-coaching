ea305d512690602f6d20f09970b5897ff86bab67
Haruna Fujita <fujitaharuna.p@gmail.com>
2025-11-10 14:55:09 +0900

    Finished 2nd challenge kinda

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,11 +6,11 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
     if @question == "I am going to work"
-      @answer = "Great!"
+      @answer = "Great"
     elsif @question.slice(-1) == "?"
-      @answer = "Silly question, get dressed and go to work!"
+      @answer = "Silly question, get dressed and go to work"
     else
-      @answer = "I don’t care, get dressed and go to work!"
+      @answer = "I don’t care, get dressed and go to work"
     end
   end
 
